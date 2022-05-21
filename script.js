f3e1960ad7258e04d6b9d3c2442ea4ad5528e23a
DerekTDR <100943733+DerekTDR@users.noreply.github.com>
2022-05-21 23:26:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ function policz() {
     var cena_ropa = 7.5;
     var cena_benzyna = 6.7;
     if (document.getElementById("benzyna").checked == true) {
-        a = cena_benzyna * parseFloat(document.getElementById("litry").value);
-        document.getElementById("wynik").value = a;
+        document.getElementById("wynik").value = cena_benzyna * parseFloat(document.getElementById("litry").value) + "zł";
     }
     else if (document.getElementById("ropa").checked == true) {
-        b = cena_ropa * parseFloat(document.getElementById("litry").value);
-        document.getElementById("wynik").value = b;
+        document.getElementById("wynik").value = cena_ropa * parseFloat(document.getElementById("litry").value) + "zł";
     }
     
 }
